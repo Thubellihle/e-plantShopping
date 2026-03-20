bb78cd2ee4405be47fab24e4f1ef47a2c406db2c
Thubelihle <127312743+Thubellihle@users.noreply.github.com>
2026-03-20 11:03:04 +0200

    Create AboutUs component for nursery details
    Add AboutUs component with nursery information

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,20 @@
+import React from "react";
 
+function AboutUs() {
+  return (
+    <div className="about">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your one-stop shop for beautiful and affordable
+        houseplants. We believe plants bring life, peace, and happiness into
+        your home.
+      </p>
+      <p>
+        Our mission is to provide high-quality plants and make plant care easy
+        for everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
